a5dec1a80c4936ccea235f9605051c5067ed8b3a
aadithjkrishna <aadithjkrishnan45@gmail.com>
2026-06-14 10:56:06 +0530

    Delete build directory

--- build/my_nav_nodes/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/turtlebot3_ws/src/my_nav_nodes
-# Build directory: /root/turtlebot3_ws/build/my_nav_nodes
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/my_nav_nodes/ament_cmake_symlink_install_targets_0_.cmake
+++
@@ -1,1 +0,0 @@
-ament_cmake_symlink_install_targets("TARGET_FILES" "/root/turtlebot3_ws/build/my_nav_nodes/wall_follower" "TARGETS" "wall_follower" "DESTINATION" "lib/my_nav_nodes")
